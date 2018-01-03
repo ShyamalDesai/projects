c75b8e78ce00eeb318d61447310869881ca00bc2
Shyamal Desai <swapnilvdesai@gmail.com>
2018-01-02 20:30:53 -0500

    Checking in incomplete BST due to machine crashing

---
+++ common/DataStructures/BST.h
@@ -0,0 +1,129 @@
+#pragma once
+#include <memory>
+template <typename KEY, typename VALUE>
+struct BSTNode
+{
+	BSTNode(const KEY key, const VALUE value)
+		: _key (key)
+		, _value(value)
+		, _parent (nullptr)
+		, _leftChild(nullptr)
+		, _rightChild(nullptr)
+	{
+		_comp = COMPARATOR();
+	}
+	const KEY _key;
+	VALUE _value;
+	BSTNode <T>* _parent;
+	BSTNode <T>* _leftChild;
+	BSTNode <T>* _rightChild;
+	
+
+private:
+	BSTNode(const BSTNode<T>&) = delete;
+	BSTNode<T>& operator =(const BSTNode<T>& rhs) = delete;
+
+
+};
+
+template <typename KEY, typename VALUE, typename COMPARATOR = std::less<KEY>>
+class BST
+{
+public:
+	BST(const KEY key, const VALUE value) 
+		: _root(new BSTNode <KEY,VALUE> (key, value))
+	{
+	}
+		
+	BST() : _root(nullptr)
+	{
+
+	}
+	~BST() {
+			removeFrom(_root);
+	}
+
+	bool get(const KEY key, const VALUE& value);
+	bool  insert(const KEY key, const VALUE& value);
+	BSTNode<KEY, VALUE>*  put(const KEY key, const VALUE& value);
+	
+private:
+	void removeFrom(BSTNode<T>* pNext)
+	{
+
+		BSTNode<T>* pref = pNext;
+		if (pref->_leftChild)
+			removeFrom(pref->_leftChild);
+		else if (pref->_rightChild)
+			removeFrom(pref->_rightChild);
+
+		if (pNext-> == _root)
+		{
+			delete pNext;
+			pNext = nullptr;
+		}
+	}
+
+	BSTNode<KEY, VALUE> *_root;
+	COMPARATOR _comp;
+};
+
+template <typename KEY, typename VALUE, typename COMPARATOR=std::less<KEY> >
+bool 
+BST<KEY, VALUE, COMPARATOR>::get(const KEY key, VALUE& value)
+{
+	bool bRet(false);
+	BSTNode<KEY, VALUE>* pNext = _root;
+	while (pNext)
+	{
+		if (pNext->_key == key)
+		{
+			value = pNext->_value;
+			bRet = true;
+			break;
+		}
+		else if (key > pNext->_key)
+		{
+			pNext = pNext->_rightChild;
+			continue;
+		}
+		else
+		{
+			pNext = pNext->_leftChild;
+		}
+	}
+	return bRet;
+}
+
+template <typename KEY, typename VALUE, typename COMPARATOR = std::less<KEY>>
+bool 
+BST<KEY, VALUE, COMPARATOR>::insert(const KEY& key, VALUE& value)
+{
+	_root = put(_root, key, value);
+}
+
+
+template <typename KEY, typename VALUE, typename COMPARATOR = std::less<KEY> >
+BST<KEY, VALUE, COMPARATOR>*
+BST<KEY, VALUE, COMPARATOR>::put(BSTNode<KEY, VALUE, COMPARATOR>* node, 
+											const KEY& key, 
+											VALUE& value)
+{
+	if (node == nullptr) {
+		return new BSTNode<KEY, VALUE, COMPARATOR>(key, value);
+	}
+	if (key == node->_key)
+	{
+		node->value = value;
+	}
+	else
+	{
+
+		bool bLess = _comp(key, node->_key);
+		if (bLess)
+			node->_leftChild = put(node->_leftChild, key, value);
+		else if (key > node->_key)
+			node->_rightChild = put(node->_rightChild, key, value);
+	}
+	return node;
+}
